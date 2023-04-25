5f5acfd3dc6dd8792b53fe1d23023eb0bd29a12b
JaviRomero00 <javier.romero.perez@iesdonana.org>
2023-04-25 14:10:50 +0200

    gasd

--- gestiones/baja.php
+++ gestiones/baja.php
@@ -23,35 +23,64 @@
             header('Location: ../login.php');
             exit;
         } else {
+            require '../conexion.php';
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                if (isset($_POST['dni'])) {
+                    $dni = $_POST['dni'];
+
+                    $clases_label = [];
+                    $clases_input = [];
+                    $error = ['dni' => []];
+
+                    foreach (['dni'] as $e) {
+                        $clases_label[$e] = '';
+                        $clases_input[$e] = '';
+                    }
+
+                    $query = "SELECT * FROM trabajadores
+                              WHERE dni = '$dni';";
+                    $resultado = pg_query($con, $query);
+
+                    if (isset($dni)) {
+                        if ($_POST['dni'] == '') {
+                            $error['dni'][] = 'Introduzca un DNI.';
+                        }else if(pg_num_rows($resultado) !== 1) {
+                            $error['dni'][] = "El trabajador no esta dado de alta.";
+                        } else if (preg_match('/^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKE]$/i', $_POST['dni']) !== 1) {
+                            $error['dni'][] = "El dni no se encuentra en el formato correcto.";
+                        }
+
+                        $vacio = true;
+
+                        foreach ($error as $err) {
+                            if (!empty($err)) {
+                                $vacio = false;
+                                break;
+                            }
+                        }
+
+                        if ($vacio) {
+                            $eliminar = "DELETE FROM trabajadores WHERE dni = '$dni'";
+                            pg_query($con, $eliminar);
+                            echo pg_last_error($con);
+                            echo "El trabajador con DNI '$dni' ha sido dado de baja con exito";
+                        }
+                    }
+                }
+            }
+        }
         ?>
 
-            <h1>Dar de Baja a un trabajador</h1>
-            <form action="baja.php" method="POST">
-                <label for="dni">Introduzca el DNI del trabajador al que debes dar de baja:</label>
-                <input type="text" name="dni" required>
-                <br>
-                <input type="submit" value="Guardar">
-            </form>
-        <?php
-            require 'conexion.php';
-            $dni = $_POST['dni'];
-            $consulta = "SELECT * FROM trabajadores
-                            WHERE dni = '$dni';";
-
-            $query = "DELETE FROM trabajadores
-                      WHERE dni = '$dni';";
-
-            $res = pg_query($con, $consulta);
-
-            if (pg_num_rows($res) == 1) {
-                pg_query($con, $query);
-                echo "<div class='aceptar'>El trabajador ha sido dado de baja con exito.</div>";
-            } else {
-                echo "<div class='error'>El DNI introducido no pertenece a ningún trabajador registrado.</div>";
-            }
-            }
-            pg_close($con);
-        ?>
+        <h1>Dar de Baja a un trabajador</h1>
+        <form method="POST">
+            <label for="dni" class="<?= $clases_label['dni'] ?>">DNI:</label>
+            <input type="text" name="dni" id="dni" class="<?= $clases_input['dni'] ?>">
+            <?php foreach ($error['dni'] as $err): ?>
+                <p><span>Error!! <?= $err ?></p>
+            <?php endforeach ?>
+            <br>
+            <input type="submit" value="Eliminar">
+        </form>
     </div>
 </body>
 </html>
